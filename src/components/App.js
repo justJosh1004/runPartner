c7b0647c3fb9c0119082865724f6cbbc398ec0a0
justJosh1004 <j.joson@live.com>
2019-02-04 14:49:08 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the homepage!</h1>
+        <h3>Look at Runs</h3>
+        <h3>Go to Profile</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
