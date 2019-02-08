408a96bb732ac071ba7e4cab3a48f2b5f87e5aec
justJosh1004 <j.joson@live.com>
2019-02-08 15:46:57 -0500

    added form validations

--- src/components/runs/RunForm.js
+++ src/components/runs/RunForm.js
@@ -8,21 +8,51 @@ import {
   FormGroup,
   Label,
   Input,
+  FormFeedback,
   FormText
 } from 'reactstrap';
 
 class RunForm extends Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <FormFeedback>{error}</FormFeedback>;
+    }
+  }
+
+  isValid({ error, touched }) {
+    if (touched && error) {
+      return false;
+    } else if (touched) {
+      return true;
+    } else {
+      return null;
+    }
+  }
+
+  isInvalid({ error, touched, pristine }) {
+    if (touched && error) {
+      return true;
+    } else if (pristine) {
+      return false;
+    } else {
+      return null;
+    }
+  }
+
   renderInput = ({ input, label, meta, id, placeholder, name, type }) => {
     return (
       <FormGroup>
         <Label for={id}>{label}</Label>
         <Input
           {...input}
-          name={name}
+          // name={name}
           type={type}
           id={id}
           placeholder={placeholder}
+          valid={this.isValid(meta)}
+          invalid={this.isInvalid(meta)}
         />
+        {this.renderError(meta)}
       </FormGroup>
     );
   };
@@ -47,7 +77,7 @@ class RunForm extends Component {
           component={this.renderInput}
           name="runLocation"
           id="theRunLocation"
-          label="Run Location"
+          label="Run Location*"
           type="text"
           placeholder="Where is the run?"
         />
@@ -57,7 +87,7 @@ class RunForm extends Component {
               component={this.renderInput}
               name="runTime"
               id="theRunTime"
-              label="Run Time"
+              label="Run Time*"
               type="time"
               placeholder="What time is the run?"
             />
@@ -67,7 +97,7 @@ class RunForm extends Component {
               component={this.renderInput}
               name="runDate"
               id="theRunDate"
-              label="Run Day"
+              label="Run Day*"
               type="date"
               placeholder="What day is the run?"
             />
@@ -79,7 +109,7 @@ class RunForm extends Component {
               component={this.renderInput}
               name="miles"
               id="theMiles"
-              label="Miles"
+              label="Miles*"
               type="number"
               placeholder="How many miles?"
             />
@@ -105,6 +135,7 @@ class RunForm extends Component {
             />
           </Col>
         </Row>
+        <FormText>* are required fields.</FormText>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button color="danger" style={{ marginRight: '10px' }}>
             Cancel
@@ -118,6 +149,22 @@ class RunForm extends Component {
 
 const validate = formValues => {
   const errors = {};
+
+  if (!formValues.runLocation) {
+    errors.runLocation = 'There must be a location.';
+  }
+  if (!formValues.runTime) {
+    errors.runTime = 'What time is the run?';
+  }
+  if (!formValues.runDate) {
+    errors.runDate = 'When is the run?';
+  }
+  if (!formValues.miles) {
+    errors.miles = 'How far is the run?';
+  }
+  if (formValues.miles < 1) {
+    errors.miles = 'The run needs to be at least 1 mile';
+  }
 
   return errors;
 };
