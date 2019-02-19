79898c18f44771154a942b04acc049074585287c
justJosh1004 <j.joson@live.com>
2019-02-19 15:37:28 -0500

    setup access to mapbox.

---
+++ src/apis/mapsToken.js
@@ -0,0 +1,2 @@
+export const TOKEN =
+  'pk.eyJ1Ijoiam9zaDEwMDQiLCJhIjoiY2pzYzZpZmFuMGhnMDQ0bDF4cDdhYnM3NCJ9.vRXxUk1HT43LnOHGg6PobQ';
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Button, Card, CardDeck, CardBody, CardHeader } from 'reactstrap';
 
+import Map from './Map';
+
 function mapStateToProps(state) {
   return {};
 }
@@ -10,30 +12,33 @@ function mapStateToProps(state) {
 class Home extends Component {
   render() {
     return (
-      <CardDeck>
-        <Card className="text-center">
-          <CardHeader>Look At My Profile</CardHeader>
-          <CardBody>
-            <Button>Go To Profile</Button>
-          </CardBody>
-        </Card>
-        <Card className="text-center">
-          <CardHeader>Look At Runs</CardHeader>
-          <CardBody>
-            <Link to="/runs">
-              <Button>Go To Runs</Button>
-            </Link>
-          </CardBody>
-        </Card>
-        <Card className="text-center">
-          <CardHeader>Create A Run</CardHeader>
-          <CardBody>
-            <Link to="/runs/create">
-              <Button>Add A Run</Button>
-            </Link>
-          </CardBody>
-        </Card>
-      </CardDeck>
+      <div>
+        <CardDeck>
+          <Card className="text-center">
+            <CardHeader>Look At My Profile</CardHeader>
+            <CardBody>
+              <Button>Go To Profile</Button>
+            </CardBody>
+          </Card>
+          <Card className="text-center">
+            <CardHeader>Look At Runs</CardHeader>
+            <CardBody>
+              <Link to="/runs">
+                <Button>Go To Runs</Button>
+              </Link>
+            </CardBody>
+          </Card>
+          <Card className="text-center">
+            <CardHeader>Create A Run</CardHeader>
+            <CardBody>
+              <Link to="/runs/create">
+                <Button>Add A Run</Button>
+              </Link>
+            </CardBody>
+          </Card>
+        </CardDeck>
+        <Map />
+      </div>
     );
   }
 }
