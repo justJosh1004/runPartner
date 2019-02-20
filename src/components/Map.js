d7e91bda5963ae10b00911e04376bfbb015be51a
justJosh1004 <j.joson@live.com>
2019-02-20 10:00:46 -0500

    installed react-map-gl-geocoder.

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 import { TOKEN } from '../apis/mapsToken';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 class Map extends Component {
   state = {
     viewport: {
       width: 400,
       height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
+      latitude: 40.4406,
+      longitude: -79.9959,
+      zoom: 10
     }
   };
 
---
+++ src/reducers/mapReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+  viewport: {
+    width: 400,
+    height: 400,
+    latitude: 40.4406,
+    longitude: -79.9959,
+    zoom: 10
+  }
+};
